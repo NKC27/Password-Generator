060d1b892e9a9c249a7d4b7418e8166bb0f5f206
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-01-08 15:57:34 +0000

    add checks to verify user has selected

--- Develop/script.js
+++ Develop/script.js
@@ -24,6 +24,35 @@ if(passLength >= 8 && passLength <= 128) {
   console.log(passLength)
 }
 
+// If password length provided is less than the min or more than the max will return an alert and require the user to start again.
+
+else {
+  alert("Password must be minimum 8 characters & maximum 128 characters")
+  return"";
+}
+
+// Asking the user to add lower case letters in the password.
+var lowerCharactersChk = confirm('Please select lower case  letters in your password? i.e (a-z)')
+
+// Asking the user to add upper case letters in the password.
+var upperCharactersChk = confirm('Please select upper case letters in your password? i.e (A-Z)')
+
+// Asking the user to add numbers in the password.
+var numberCharactersChk = confirm('Please add numbers in your password? i.e.(0-9)')
+
+// Asking the user to add special characters in the password.
+var specialCharactersChk = confirm('Please add special characters  letters in your password? i.e. (!#$%&\'()*+,-./:;<=>?@[]^_`{|}~)')
+
+// If lower case letters are selected from prompts, random pick a letter from the lowerCharaters array.
+if (lowerCharactersChk) {
+  allChars += lowerCharacters
+}
+
+// If upper case letters are selected, random pick a letter from the upperCharacters array.
+if (upperCharactersChk) {
+  allChars += upperCharacters
+}
+
 // Output password to the #password input
 function writePassword() {
   var password = generatePassword();
